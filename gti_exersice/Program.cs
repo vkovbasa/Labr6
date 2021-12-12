44f94c838f98c526740703c7864a5e109f8c93aa
vkovbasa <vkovbasa88@gmail.com>
2021-12-12 20:01:32 +0200

    Створено найпростіший консольний додаток

---
+++ gti_exersice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gti_exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
